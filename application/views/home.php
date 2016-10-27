e5e007e4425f4779ee30c0c1e6bb6b7452e16aba
A-ron <aaron.elliott@kihomac.com>
2016-10-27 16:50:32 -0500

    home.php

--- application/views/home.php
+++ application/views/home.php
@@ -64,31 +64,28 @@
     <header>
         <div class="container">
             <div class="row">
-                <div class="col-sm-5">
+                <div class="col-sm-7">
                     <div class="header-content">
                         <div class="header-content-inner">
                             <h2>Get Started - It's Free</h2>
                             <form>
                                 <div class="form-group row">
-                                <label for="inputName" class="col-sm-2 col-form-label">First Name</label>
-                                <div class="col-sm-10">
+                                <div class="col-sm-12">
                                     <input type="email" class="form-control" id="inputName" placeholder="First Name">
                                 </div>
                                 </div>
                                 <div class="form-group row">
-                                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                                <div class="col-sm-10">
+                                <div class="col-sm-12">
                                     <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                                 </div>
                                 </div>
                                 <div class="form-group row">
-                                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                                <div class="col-sm-10">
+                                <div class="col-sm-12">
                                     <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
                                 </div>
                                 </div>
                                 <fieldset class="form-group row">
-                                    <legend class="col-form-legend col-sm-2">Options</legend>
+                                    <div class="col-sm-2"></div>
                                     <div class="col-sm-10">
                                         <div class="form-check">
                                         <label class="form-check-label">
@@ -113,7 +110,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-7">
+                <div class="col-sm-5">
                 <div class="header-content">
                         <div class="header-content-inner">
                             <h1>Plan & Organize Your Daily Life Better</h1>
