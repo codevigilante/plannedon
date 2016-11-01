65b63726d07cff7178030309c541ade05893f100
A-ron <aaron.elliott@kihomac.com>
2016-11-01 10:14:08 -0500

    fixcing;

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE"); 
+header("Access-Control-Allow-Methods: *"); 
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
