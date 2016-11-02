be80a3fe827e60ae1b9521c1452266242db7c80a
A-ron <aaron.elliott@kihomac.com>
2016-11-02 13:59:30 -0500

    testing

--- index.php
+++ index.php
@@ -35,10 +35,12 @@ if (defined('ENVIRONMENT'))
 		case 'local':
 		case 'development':
 			error_reporting(E_ALL);
+			ini_set("display_errors", 1);
 			break;
 
 		case 'production':
 			error_reporting(0);
+			ini_set("display_errors", 0);
 			break;
 
 		default:
