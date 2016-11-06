feea1b4613d6dc63a803cae0ce899788677403e9
A-ron <codevigilante@gmail.com>
2016-11-06 11:00:31 -0600

    testing 404

--- application/config/config.php
+++ application/config/config.php
@@ -375,7 +375,7 @@ $config['rewrite_short_tags'] = FALSE;
 | Comma-delimited, e.g. '10.0.1.200,10.0.1.201'
 |
 */
-$config['proxy_ips'] = getenv('OPENSHIFT_HAPROXY_IP') ?: '';
+$config['proxy_ips'] = '';
 
 
 /* End of file config.php */
