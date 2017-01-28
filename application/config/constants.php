9cb514233bd27d94148d69edc5696620e5f26e7d
A-ron <codevigilante@gmail.com>
2017-01-28 11:33:25 -0600

    v0.4: improved js; move between days;

--- application/config/constants.php
+++ application/config/constants.php
@@ -4,7 +4,7 @@
     APP_VERSION - the current version of Planned On
 */
 
-define("APP_VERSION", "v0.3");
+define("APP_VERSION", "v0.4");
 
 /*
 |--------------------------------------------------------------------------
